c586ff1806bc9e42e3acbc847cbf57ce3b4b8eb7
hamsoace <hamsoace@gmail.com>
2020-09-10 08:57:58 +0300

    book form

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-$("#booking-form").validate();
- 
-$("#checkin").datepicker({
-    defaultDate: "+1w",
-    changeMonth: false,
-    numberOfMonths: 1,
-    prevText: '<i class="fa fa-chevron-left"></i>',
-    nextText: '<i class="fa fa-chevron-right"></i>',
-    onClose: function( selectedDate ) {
-        $( "#checkout" ).datepicker( "option", "minDate", selectedDate );
-    }
-});
- 
 $("#checkout").datepicker({
     defaultDate: "+1w",
     changeMonth: false,
