11b8b2a6a7ac995587b96eb56ff30ae89fb0ec73
hamsoace <hamsoace@gmail.com>
2020-09-10 08:49:30 +0300

    forms changed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
+$("#booking-form").validate();
+ 
+$("#checkin").datepicker({
+    defaultDate: "+1w",
+    changeMonth: false,
+    numberOfMonths: 1,
+    prevText: '<i class="fa fa-chevron-left"></i>',
+    nextText: '<i class="fa fa-chevron-right"></i>',
+    onClose: function( selectedDate ) {
+        $( "#checkout" ).datepicker( "option", "minDate", selectedDate );
+    }
+});
+ 
+$("#checkout").datepicker({
+    defaultDate: "+1w",
+    changeMonth: false,
+    numberOfMonths: 1,
+    prevText: '<i class="fa fa-chevron-left"></i>',
+    nextText: '<i class="fa fa-chevron-right"></i>',            
+    onClose: function( selectedDate ) {
+        $( "#checkin" ).datepicker( "option", "maxDate", selectedDate );
+    }
 });
