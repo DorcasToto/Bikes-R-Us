7b568d35e8d490c7d48dacfa0cfbecc066497a5d
hamsoace <hamsoace@gmail.com>
2020-09-11 11:31:43 +0300

    styled and functionality the display part

--- js/app.js
+++ js/app.js
@@ -13,6 +13,24 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function () {
+  $(".popup-btn").click(function () {
+    $(".popup")
+      .css("display", "flex")
+      .css("animation", "popup-bg 1s")
+      .css("animation-interaction-count", "1");
+    $(".popup-wrap")
+      .css("animation", "popup 1s")
+      .css("animation-iteration-count", "1");
+  });
+
+  $(".close").click(function () {
+    $(".popup-wrap")
+      .css("animation", "popup 1s")
+      .css("animation-iteration-count", "1");
+    $(".popup").delay(500).fadeOut();
+  });
+});
 
 $(document).ready(function(){
   function Book(mechanic, service, location){
